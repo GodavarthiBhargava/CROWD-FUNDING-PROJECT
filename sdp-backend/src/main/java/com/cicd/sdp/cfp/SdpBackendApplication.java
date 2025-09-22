124be5f577b80c2580e7189c59b67ed2d24c676f
BHARGAVA <godavarthibhargava05@gmail.com>
2025-09-22 22:53:52 +0530

    pushed

---
+++ sdp-backend/src/main/java/com/cicd/sdp/cfp/SdpBackendApplication.java
@@ -0,0 +1,15 @@
+package com.cicd.sdp.cfp;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdpBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdpBackendApplication.class, args);
+		System.out.println("Project Started");
+			}
+
+}
---
+++ sdp-backend/src/test/java/com/cicd/sdp/cfp/SdpBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cicd.sdp.cfp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdpBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
